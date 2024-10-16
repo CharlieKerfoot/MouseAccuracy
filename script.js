80d128aed8f46f2568d9645a90ee96d47941eb86
yzsythsbt <helena_zhang@horacemann.org>
2024-10-16 17:17:44 -0400

    timer draft (any member can modify if needed)

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-// Start Game
+// Timer
 
+document.getElementById("start").addEventListener("click", () => {
+    document.getElementById("start").innerText = "GAME ONGOING";
 
+    let remaining = parseInt(document.getElementById("timer").innerText);
+    let timer = setInterval(function () {
+        if (remaining > 0) {
+            remaining--;
+            document.getElementById("timer").innerText = remaining;
+        } else {
+            clearInterval(timer);
+            document.getElementById("start").innerText = "GAME FINISHED";
+        }
 
-// Targets Appear
+    }, 1000);
 
-
-
-// Targets Disappear 
+});
