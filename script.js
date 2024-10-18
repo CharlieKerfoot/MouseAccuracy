e9e6eeb4427f49ab759396860f3357600c965dda
Charlie Kerfoot <charliekerfoot@Charlies-MacBook-Air-2.local>
2024-10-18 14:05:46 -0400

    added spawning targets

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.getElementById("start").addEventListener("click", () => {
         if (remaining > 0) {
             remaining--;
             document.getElementById("timer").innerText = remaining;
+            spawnTarget();
         } else {
             clearInterval(timer);
             document.getElementById("start").innerText = "GAME FINISHED";
@@ -16,3 +17,17 @@ document.getElementById("start").addEventListener("click", () => {
     }, 1000);
 
 });
+
+// spawn targets
+
+function spawnTarget() {
+    if(!! document.getElementById("target")) document.getElementById("target").remove();
+    const target = document.createElement("button");
+    target.style.height = "100px"
+    target.style.width = "100px"
+    target.style.position = "absolute";
+    target.style.left = `${Math.random() * 80 + 10}vw`;
+    target.style.top = `${Math.random() * 80 + 10}vh`;
+    target.id = "target"
+    document.body.appendChild(target);
+}
